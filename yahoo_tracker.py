d9ea698b30c9ababfcade0c7116fb0a380309a87
rc2000123 <toametru2000@gmail.com>
2022-08-04 10:29:40 +0800

    added catch exception for news

--- yahoo_tracker.py
+++ yahoo_tracker.py
@@ -54,22 +54,24 @@ with db.connect(logger) as conn:
             continue
 
         for news in news_list:
-            published_at = news['published_at']   
-            if published_at.endswith(" 分鐘前"):
-                published_at = published_at.removesuffix(' 分鐘前')
-                published_at = epoch_time - int(published_at) * 60
-            elif published_at.endswith("幾秒前"):
-                #published_at = published_at.removesuffix(' 幾秒鐘前')
-                published_at = epoch_time
-            elif published_at.endswith(" 小時前"):
-                published_at = published_at.removesuffix(" 小時前")
-                published_at = epoch_time - int(published_at) * 60 * 60
+            try:
+                published_at = news['published_at']   
+                if published_at.endswith(" 分鐘前"):
+                    published_at = published_at.removesuffix(' 分鐘前')
+                    published_at = epoch_time - int(published_at) * 60
+                elif published_at.endswith("幾秒前"):
+                    #published_at = published_at.removesuffix(' 幾秒鐘前')
+                    published_at = epoch_time
+                elif published_at.endswith(" 小時前"):
+                    published_at = published_at.removesuffix(" 小時前")
+                    published_at = epoch_time - int(published_at) * 60 * 60
 
-            else:
-                raise Exception(published_at + " published_at parsing error") 
-            if(db.insertPost(conn,news['id'],news['provider_id'],news['provider_name'],published_at,news['summary'],news['title'],news['url'],datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))):
-                logger.info("inserted: " + news['title'] + "at: " + datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))
-            
+                else:
+                    raise Exception(published_at + " published_at parsing error") 
+                if(db.insertPost(conn,news['id'],news['provider_id'],news['provider_name'],published_at,news['summary'],news['title'],news['url'],datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))):
+                    logger.info("inserted: " + news['title'] + "at: " + datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))
+            except:
+                logger.error("Error in news insertion, continuing for now")
             #sql_str = "INSERT INTO yahoo_posts (id,provider_id,provider_name,published_at,summary,title,url,time) VALUES ('{id}','{provider_id}','{name}','{published_at}','{summary}','{title}','{url}','{time}')".format(id = news['id'], provider_id = news['provider_id'],name = news['provider_name'],published_at = published_at,summary = news['summary'],title = news['title'],url = news['url'],time = datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))
             #if(db.executeSQL(sql_str)):
             #    logger.info("inserted: " + news['title'] + "at: " + datetime.datetime.fromtimestamp(published_at).strftime("%m/%d/%Y, %H:%M:%S"))
